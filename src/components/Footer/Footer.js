6175cbff8230339d23124c322ff4dc0587ce0299
Polly Liu <polly.liu4@gmail.com>
2023-01-05 03:13:01 -0500

    Finished footer and global theme

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import "./Footer.css"
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 class Footer extends React.Component {
     render() {
         return(
             <Box className="footer">
-                <Box className="box"> Copyright &copy; 2023 Polly Liu. All Rights Reserved</Box>
+                <Flex className="icons">
+                    <a className="email" href="mailto: pollyliu4@gmail.com" target="_blank">
+                        <i class="far fa-envelope"></i>
+                        </a>
+                    <a className="github" href="https://github.com/pollyliu1" target="_blank">
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a className="linkedin" href="https://www.linkedin.com/in/pollyliu1/" target="_blank">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </Flex>
+                <Box className="copyright">
+                    Copyright &copy; 2023 Polly Liu. All Rights Reserved
+                </Box>
             </Box>
         )
     }
