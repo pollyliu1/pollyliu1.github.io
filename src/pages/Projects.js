6d9998e862c0c3d179d3e8231c0eaf977a5b5568
Polly Liu <polly.liu4@gmail.com>
2023-01-06 04:05:09 -0500

    Updated projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -21,7 +21,7 @@ class Projects extends React.Component {
     render() {
         return(
             <Flex className="projects">
-                <Card className="card" maxW='sm'>
+                <Card className="card uwstream" maxW='sm'>
                     <CardBody>
                         <Image
                         src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
@@ -29,14 +29,11 @@ class Projects extends React.Component {
                         borderRadius='lg'
                         />
                         <Stack mt='6' spacing='3'>
-                        <Heading size='md'>Living room Sofa</Heading>
+                        <Heading size='md'>UWStream</Heading>
                         <Text>
                             This sofa is perfect for modern tropical spaces, baroque inspired
                             spaces, earthy toned spaces and for people who love a chic design with a
                             sprinkle of vintage design.
-                        </Text>
-                        <Text color='blue.600' fontSize='2xl'>
-                            $450
                         </Text>
                         </Stack>
                     </CardBody>
@@ -52,7 +49,62 @@ class Projects extends React.Component {
                         </ButtonGroup>
                     </CardFooter>
                 </Card>
-                <Text className="card">hi</Text>
+                <Card className="card zooveniers" maxW='sm'>
+                    <CardBody>
+                        <Image
+                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                        alt='Green double couch with wooden legs'
+                        borderRadius='lg'
+                        />
+                        <Stack mt='6' spacing='3'>
+                        <Heading size='md'>Zooveniers</Heading>
+                        <Text>
+                            This sofa is perfect for modern tropical spaces, baroque inspired
+                            spaces, earthy toned spaces and for people who love a chic design with a
+                            sprinkle of vintage design.
+                        </Text>
+                        </Stack>
+                    </CardBody>
+                    <Divider />
+                    <CardFooter>
+                        <ButtonGroup spacing='2'>
+                        <Button variant='solid' colorScheme='blue'>
+                            Buy now
+                        </Button>
+                        <Button variant='ghost' colorScheme='blue'>
+                            Add to cart
+                        </Button>
+                        </ButtonGroup>
+                    </CardFooter>
+                </Card>
+                <Card className="card website" maxW='sm'>
+                    <CardBody>
+                        <Image
+                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                        alt='Green double couch with wooden legs'
+                        borderRadius='lg'
+                        />
+                        <Stack mt='6' spacing='3'>
+                        <Heading size='md'>Personal Website</Heading>
+                        <Text>
+                            This sofa is perfect for modern tropical spaces, baroque inspired
+                            spaces, earthy toned spaces and for people who love a chic design with a
+                            sprinkle of vintage design.
+                        </Text>
+                        </Stack>
+                    </CardBody>
+                    <Divider />
+                    <CardFooter>
+                        <ButtonGroup spacing='2'>
+                        <Button variant='solid' colorScheme='blue'>
+                            Buy now
+                        </Button>
+                        <Button variant='ghost' colorScheme='blue'>
+                            Add to cart
+                        </Button>
+                        </ButtonGroup>
+                    </CardFooter>
+                </Card>
             </Flex>
         )
     }
