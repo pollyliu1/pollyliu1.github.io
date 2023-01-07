159081d52361fcbeca471a8976d85d6fe071289c
Polly Liu <polly.liu4@gmail.com>
2023-01-07 01:26:46 -0500

    Finished projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -24,85 +24,64 @@ class Projects extends React.Component {
                 <Card className="card uwstream" maxW='sm'>
                     <CardBody>
                         <Image
-                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                        alt='Green double couch with wooden legs'
+                        src='under_construction.png'
+                        alt='under construction image'
                         borderRadius='lg'
                         />
                         <Stack mt='6' spacing='3'>
                         <Heading size='md'>UWStream</Heading>
                         <Text>
-                            This sofa is perfect for modern tropical spaces, baroque inspired
-                            spaces, earthy toned spaces and for people who love a chic design with a
-                            sprinkle of vintage design.
+                            Visual co-op sequences for University of Waterloo students
                         </Text>
                         </Stack>
                     </CardBody>
                     <Divider />
                     <CardFooter>
-                        <ButtonGroup spacing='2'>
-                        <Button variant='solid' colorScheme='blue'>
-                            Buy now
-                        </Button>
-                        <Button variant='ghost' colorScheme='blue'>
-                            Add to cart
-                        </Button>
-                        </ButtonGroup>
+                        <Box className="gh-icons">
+                            <a href="https://github.com/pollyliu1/UWStream" target="_blank"><i class="fab fa-github"></i></a>
+                        </Box>
                     </CardFooter>
                 </Card>
                 <Card className="card zooveniers" maxW='sm'>
                     <CardBody>
                         <Image
-                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                        alt='Green double couch with wooden legs'
+                        src='under_construction.png'
+                        alt='under construction image'
                         borderRadius='lg'
                         />
                         <Stack mt='6' spacing='3'>
                         <Heading size='md'>Zooveniers</Heading>
                         <Text>
-                            This sofa is perfect for modern tropical spaces, baroque inspired
-                            spaces, earthy toned spaces and for people who love a chic design with a
-                            sprinkle of vintage design.
+                            Zookeeper game made with Unity (WIP)
                         </Text>
                         </Stack>
                     </CardBody>
                     <Divider />
                     <CardFooter>
-                        <ButtonGroup spacing='2'>
-                        <Button variant='solid' colorScheme='blue'>
-                            Buy now
-                        </Button>
-                        <Button variant='ghost' colorScheme='blue'>
-                            Add to cart
-                        </Button>
-                        </ButtonGroup>
+                        <Box className="gh-icons">
+                            <a href="https://github.com/pollyliu1/Zooveniers" target="_blank"><i class="fab fa-github"></i></a>
+                        </Box>
                     </CardFooter>
                 </Card>
                 <Card className="card website" maxW='sm'>
                     <CardBody>
                         <Image
-                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                        alt='Green double couch with wooden legs'
+                        src='under_construction.png'
+                        alt='under construction image'
                         borderRadius='lg'
                         />
                         <Stack mt='6' spacing='3'>
                         <Heading size='md'>Personal Website</Heading>
                         <Text>
-                            This sofa is perfect for modern tropical spaces, baroque inspired
-                            spaces, earthy toned spaces and for people who love a chic design with a
-                            sprinkle of vintage design.
+                            View the source code behind this website :)
                         </Text>
                         </Stack>
                     </CardBody>
                     <Divider />
                     <CardFooter>
-                        <ButtonGroup spacing='2'>
-                        <Button variant='solid' colorScheme='blue'>
-                            Buy now
-                        </Button>
-                        <Button variant='ghost' colorScheme='blue'>
-                            Add to cart
-                        </Button>
-                        </ButtonGroup>
+                        <Box className="gh-icons">
+                            <a href="https://github.com/pollyliu1/pollyliu1.github.io" target="_blank"><i class="fab fa-github"></i></a>
+                        </Box>
                     </CardFooter>
                 </Card>
             </Flex>
