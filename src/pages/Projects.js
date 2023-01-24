d6c04ddd802b9571c741ad0da693bff565cb64ba
Polly Liu <polly.liu4@gmail.com>
2023-01-24 12:18:52 -0500

    updated projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -45,6 +45,81 @@ function Projects() {
                     </Box>
                 </CardFooter>
             </Card>
+            <Card className="card empowrher" maxW='sm'>
+                <CardBody>
+                    <Image
+                    src='EmpowrHER.png'
+                    alt='empowrher homepage'
+                    borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                    <Text className="headers" size='md'>EmpowrHER</Text>
+                    <Text>
+                        Skill trading web app for women in STEM
+                        <br></br>
+                    </Text>
+                    </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                    <Box className="gh-icons">
+                        <a href="https://github.com/pollyliu1/EmpowrHER"
+                        target="_blank" rel="noreferrer">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </Box>
+                </CardFooter>
+            </Card>
+            <Card className="card website" maxW='sm'>
+                <CardBody>
+                    <Image
+                    src='Logo16by9.png'
+                    alt='personal website logo'
+                    borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                    <Text className="headers" size='md'>Personal Website</Text>
+                    <Text>
+                        View the source code behind this website :)
+                        <br></br>
+                    </Text>
+                    </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                    <Box className="gh-icons">
+                        <a href="https://github.com/pollyliu1/pollyliu1.github.io"
+                        target="_blank" rel="noreferrer">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </Box>
+                </CardFooter>
+            </Card>
+            <Card className="card cipher" maxW='sm'>
+                <CardBody>
+                    <Image
+                    src='Cipher.png'
+                    alt='cipher.exe logo'
+                    borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                    <Text className="headers" size='md'>Cipher.exe</Text>
+                    <Text>
+                        Cryptography program that encrypts plaintext and decrypts ciphertext
+                        <br></br>
+                    </Text>
+                    </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                    <Box className="gh-icons">
+                        <a href="https://github.com/pollyliu1/Cipher.exe"
+                        target="_blank" rel="noreferrer">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </Box>
+                </CardFooter>
+            </Card>
             <Card className="card zooveniers" maxW='sm'>
                 <CardBody>
                     <Image
@@ -64,31 +139,6 @@ function Projects() {
                 <CardFooter>
                     <Box className="gh-icons">
                         <a href="https://github.com/pollyliu1/Zooveniers"
-                        target="_blank" rel="noreferrer">
-                            <i class="fab fa-github"></i>
-                        </a>
-                    </Box>
-                </CardFooter>
-            </Card>
-            <Card className="card website" maxW='sm'>
-                <CardBody>
-                    <Image
-                    src='Logo16by9.png'
-                    alt='under construction image'
-                    borderRadius='lg'
-                    />
-                    <Stack mt='6' spacing='3'>
-                    <Text className="headers" size='md'>Personal Website</Text>
-                    <Text>
-                        View the source code behind this website :)
-                        <br></br>
-                    </Text>
-                    </Stack>
-                </CardBody>
-                <Divider />
-                <CardFooter>
-                    <Box className="gh-icons">
-                        <a href="https://github.com/pollyliu1/pollyliu1.github.io"
                         target="_blank" rel="noreferrer">
                             <i class="fab fa-github"></i>
                         </a>
