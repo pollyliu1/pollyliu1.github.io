60a2f407de43bc3abfeb16433542086c7e4e88f7
Polly Liu <polly.liu4@gmail.com>
2023-02-22 15:03:28 -0500

    fixed footer flexbox

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import "./Footer.css"
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react'
+import { motion } from "framer-motion"
 
 function Footer() {
     return(
         <Box className="footer">
             <Flex className="icons">
-                <a className="email" href="mailto: pollyliu4@gmail.com"
-                target="_blank" rel="noreferrer">
-                    <i class="far fa-envelope"></i>
-                </a>
-                <a className="github" href="https://github.com/pollyliu1"
-                target="_blank" rel="noreferrer">
-                    <i class="fab fa-github"></i>
-                </a>
-                <a className="linkedin" href="https://www.linkedin.com/in/pollyliu1/"
-                target="_blank" rel="noreferrer">
-                    <i class="fab fa-linkedin-in"></i>
-                </a>
+                <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}>
+                    <a className="email footer-icon" href="mailto: pollyliu4@gmail.com"
+                    target="_blank" rel="noreferrer">
+                        <i class="far fa-envelope"></i>
+                    </a>
+                </motion.div>
+                <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}>
+                    <a className="github footer-icon" href="https://github.com/pollyliu1"
+                    target="_blank" rel="noreferrer">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </motion.div>
+                <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}>
+                    <a className="linkedin footer-icon" href="https://www.linkedin.com/in/pollyliu1/"
+                    target="_blank" rel="noreferrer">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </motion.div>
             </Flex>
             <Box className="copyright">
                 Copyright &copy; 2023 Polly Liu. All Rights Reserved
